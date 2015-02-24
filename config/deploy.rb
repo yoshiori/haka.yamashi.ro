3cfa8facd26957b3ca758a8088c7a84c8cf174ce
yoshiori <yoshiori@gmail.com>
2015-02-24 15:37:25 +0900

    :bug: Fix cap setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      invoke 'unicorn:reload'
+      invoke 'unicorn:restart'
     end
   end
 
