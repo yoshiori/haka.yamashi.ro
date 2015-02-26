8807e4643f78d6f6f21756e4694bae6d328b09d2
yoshiori <yoshiori@gmail.com>
2015-02-26 13:11:18 +0900

    :heavy_plus_sign: Add User page link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(nickname: params[:id])
+    @user = User.find_by(nickname: params[:nickname])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # omniauth
   get "/auth/:provider/callback", to: "sessions#create"
 
-  delete "/signout" => "sessions#destroy", :as => :signout
+  delete "/signout" => "sessions#destroy", as: :signout
 
   resources :incenses, only: :create
 
-  get "/@:id" => "users#show"
+  get "/@:nickname" => "users#show", as: :user
 end
