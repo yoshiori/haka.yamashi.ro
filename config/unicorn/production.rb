6ed8d69132b30d7937c7f0154455d5498a7c7748
yoshiori <yoshiori@gmail.com>
2015-03-02 21:17:22 +0900

    :bug: Fix unicorn setting

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -16,20 +16,35 @@ pid "#{RAILS_ROOT}/tmp/pids/unicorn.pid"
 stderr_path "#{RAILS_ROOT}/log/unicorn.log"
 stdout_path "#{RAILS_ROOT}/log/unicorn.log"
 
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{RAILS_ROOT}/Gemfile"
+end
+
+# preload
 preload_app true
-# check_client_connection false
 
 before_fork do |server, worker|
-  old_pid = "#{RAILS_ROOT}/tmp/pids/unicorn.pid.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
     end
   end
 end
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
+  end
 end
