92481ff555862b9f72fabacea97df1b88a312d45
yoshiori <yoshiori@gmail.com>
2015-03-02 21:23:04 +0900

    :rocket: Fix n+1 to incenses(index)

--- app/controllers/incenses_controller.rb
+++ app/controllers/incenses_controller.rb
@@ -12,7 +12,7 @@ class IncensesController < ApplicationController
   end
 
   def index
-    @incenses = Incense.all.recent.page(params[:page])
+    @incenses = Incense.recent.includes(:user).page(params[:page])
   end
 
   private
